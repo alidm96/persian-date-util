ebe943c1c6a176e0f142441e789170901d9e5b65
dahmardeh.a <dahmardeh.a@tiddev.com>
2025-05-24 16:41:46 +0330

    Initial commit

---
+++ src/test/java/com/tiddev/persian_date_util/util/PersianDateUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiddev.persian_date_util.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersianDateUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
